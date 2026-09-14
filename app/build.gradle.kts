528b0a92cfb6a1b0c7f9a5b1cf89375cb1f3f09f
UnrealSalty <eswartmansalts@gmail.com>
2026-09-14 20:01:50 +0200

    chore(release): bump version to 0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         // targetSdk 25: keeps parked surveillance free of O background limits.
         minSdk = 28
         targetSdk = 25
-        versionCode = 5
-        versionName = "0.5"
+        versionCode = 6
+        versionName = "0.6"
 
         externalNativeBuild { cmake { arguments += "-DANDROID_STL=none" } }
     }
